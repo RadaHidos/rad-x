a91b9cf623c3310ba7a372ccd0c97b97a678868d
RadaHidos <120414438+RadaHidos@users.noreply.github.com>
2026-01-05 11:58:29 +0200

    First Commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+export const Home = () => {
+  return (
+    <div className="min-h-screen ">
+      {/* Theme Toggle */}
+      {/* Background effects */}
+      {/* NavBar */}
+      {/* Main Content */}
+      {/* Footer */}
+    </div>
+  );
+};
